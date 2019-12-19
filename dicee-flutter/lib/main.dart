a903ad3f23b17e065770159d9e6084162023eb7e
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2019-12-19 15:04:18 +0530

    Flutter_Development_gesture_detection_with_flutter_button_widget_update_v2.1

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -24,14 +24,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('LEFT BUTTON');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: (){
+                print('RIGHT BUTTON');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
