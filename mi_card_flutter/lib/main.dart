f20a824bb7a8533034c0b24e0aff5d26a59d44f4
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2019-12-19 11:15:58 +0530

    Flutter_Development_material_icons_update_v1.6

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey[900],
+        backgroundColor: Colors.teal[900],
         body: SafeArea(
             child: Column(
           children: <Widget>[
@@ -20,20 +20,63 @@ class MyApp extends StatelessWidget {
             Text(
               'Angel',
               style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-              ),
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
             ),
             Text(
               'FLUTTER DEVELOPER',
               style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                fontSize: 20.0,
-                color: Colors.teal.shade100,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.5
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '+91 8009 098 453',
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('angel@em.io',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal,
+                    fontFamily: 'Sans Source Pro'
+                  ),)
+                ],
               ),
             )
           ],
