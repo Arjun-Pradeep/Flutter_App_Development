d1d88070817fb5f8a3afa7bab2a1409b3d273296
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2019-12-19 14:26:48 +0530

    Flutter_Development_expanded_widegt_update_v1.9

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -6,7 +6,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+            child: Text('Dicee'),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +20,15 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+      ],
+    );
   }
 }
