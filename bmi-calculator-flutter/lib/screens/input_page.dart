2b66fc6fe9fc312a00cf6e8032d34874324d38f5
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2019-12-25 21:07:53 +0530

    Flutter_Development_bmi_calculator_app_update_v3.7

--- bmi-calculator-flutter/lib/screens/input_page.dart
+++ bmi-calculator-flutter/lib/screens/input_page.dart
@@ -1,10 +1,11 @@
-import 'package:bmi_calculator/results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'icon_content.dart';
-import 'reusable_card.dart';
-import 'constants.dart';
+import 'package:bmi_calculator/components/icon_content.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/constants.dart';
 import 'results_page.dart';
+import 'package:bmi_calculator/components/button_button.dart';
+import 'package:bmi_calculator/components/round_icon_button.dart';
 
 enum Gender { male, female }
 
@@ -191,42 +192,17 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          GestureDetector(
+          BottomButton(
+            buttonTitle: 'CALCULATE',
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                  builder: (context) => ResultsPage()),
+                MaterialPageRoute(builder: (context) => ResultsPage()),
               );
             },
-            child: Container(
-                child: Text('CALCULATE'),
-                color: kbottomContainerColor,
-                margin: EdgeInsets.only(top: 10.0),
-                width: double.infinity,
-                height: kbottomContainerHeight),
           ),
         ],
       ),
-    );
-  }
-}
-
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({@required this.icon, @required this.onPressed});
-
-  final IconData icon;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 6.0,
-      constraints: BoxConstraints.tightFor(width: 56.0, height: 56.0),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
---
+++ bmi-calculator-flutter/lib/screens/results_page.dart
@@ -0,0 +1,61 @@
+import 'package:bmi_calculator/components/button_button.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI Calculator'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(15.0),
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  'Your Result',
+                  style: ktitleTextStyle,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: ReusableCard(
+                colour: kactiveCardColor,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Normal',
+                      style: kResultTextStyle,
+                    ),
+                    Text(
+                      '18.3',
+                      style: kBMITextStyle,
+                    ),
+                    Text(
+                      'Your BMI Result is quite low. you should eat more!',
+                      textAlign: TextAlign.center,
+                      style: kBodyTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            BottomButton(
+              buttonTitle: 'RECALCULATE',
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ));
+  }
+}
