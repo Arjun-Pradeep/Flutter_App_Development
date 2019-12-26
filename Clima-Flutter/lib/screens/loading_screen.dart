f2c57a3a656d1fb366490fea20bca70f9657ca7d
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2019-12-26 16:16:02 +0530

    Flutter_Development_clima_app_json_parsing_update_v4.3

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -21,8 +23,32 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async{
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if(response.statusCode == 200){
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      int  condition = decodedData['weather'][0]['id'];
+
+      double temperature = decodedData['main']['temp'];
+
+      String cityName = decodedData['name'];
+      
+      print(condition);
+      print(temperature);
+      print(cityName);
+
+    }else{
+      print(response.statusCode); 
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
 
     );
